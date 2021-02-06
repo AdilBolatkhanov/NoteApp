dd59b61aa333028e7a983bfb95ac137e878ad41c
AdilBolatkhanov <adil7701@mail.ru>
2021-02-06 12:30:18 +0600

    setup SSL and HTTPS support

--- app/src/main/java/com/example/noteapp/util/Constants.kt
+++ app/src/main/java/com/example/noteapp/util/Constants.kt
@@ -14,7 +14,7 @@ object Constants {
 
     const val DATABASE_NAME = "notes-database"
 
-    const val BASE_URL = "http://192.168.100.2:8080"
+    const val BASE_URL = "https://192.168.100.2:8002"
 
     const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
 
